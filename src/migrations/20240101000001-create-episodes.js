e9681aa11360e379a7ecbeb1e2c731b1b4275467
TySteamTest <130309211+TySteamTest@users.noreply.github.com>
2026-02-06 14:49:41 -0500

    Fix episodes migration - use UUID not INTEGER to match model

--- src/migrations/20240101000001-create-episodes.js
+++ src/migrations/20240101000001-create-episodes.js
@@ -4,9 +4,9 @@ module.exports = {
   up: async (queryInterface, Sequelize) => {
     await queryInterface.createTable('episodes', {
       id: {
-        type: Sequelize.INTEGER,
+        type: Sequelize.UUID,
         primaryKey: true,
-        autoIncrement: true,
+        defaultValue: Sequelize.UUIDV4,
         allowNull: false,
       },
       showName: {
